f9be6a7d50269081530c498e420bc4f5b774984a
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-05-01 17:41:21 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 1572 ms, Memory: 27320 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.*;
+
+public class Main{
+	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		
+		
+		for(int j=1; j<=n; j++) {
+			for(int i=1; i<=3*n; i++) {
+				System.out.print("@");
+			}
+			for(int i=1; i<=n; i++) {
+				System.out.print(" ");
+			}
+			for(int i=1; i<=n; i++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		for(int j=1; j<=3*n; j++) {
+			for(int i=1; i<=n; i++) {
+				System.out.print("@");
+			}
+			for(int i=1; i<=n; i++) {
+				System.out.print(" ");
+			}
+			for(int i=1; i<=n; i++) {
+				System.out.print("@");
+			}
+			for(int i=1; i<=n; i++) {
+				System.out.print(" ");
+			}
+			for(int i=1; i<=n; i++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		for(int j=1; j<=n; j++) {
+			for(int i=1; i<=n; i++) {
+				System.out.print("@");
+			}
+			for(int i=1; i<=n; i++) {
+				System.out.print(" ");
+			}
+			for(int i=1; i<=3*n; i++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+
+		
+
+	}
+}
