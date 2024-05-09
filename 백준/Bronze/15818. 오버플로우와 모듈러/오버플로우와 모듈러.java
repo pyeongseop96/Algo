1fc74796d614f00af0d88fe4fa89fadf9b15637c
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-05-10 08:47:11 +0900

    [Bronze III] Title: 오버플로우와 모듈러, Time: 84 ms, Memory: 11684 KB -BaekjoonHub

---
+++ 백준/Bronze/15818. 오버플로우와 모듈러/오버플로우와 모듈러.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Main{
+
+    public static void main(String args[]) {
+        FastScanner sc = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        long ans = 1;
+        for(int i=1; i<=N; i++) {
+        	ans *= sc.nextLong();
+        	ans %= M;
+        }
+
+        out.println(ans);
+        out.close();
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
