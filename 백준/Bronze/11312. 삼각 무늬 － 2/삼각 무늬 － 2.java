02680ba257b3c82ac3c31d0dccdfc215d3fae5c7
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-05-18 12:58:45 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 100 ms, Memory: 12188 KB -BaekjoonHub

---
+++ 백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	public static void main(String args[]) {
+		FastScanner sc = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = sc.nextInt();
+		for(int tc=1; tc<=T; tc++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			int C = A/B;
+			long ans = 0;
+			for(int i=1; i<=C; i++) {
+				ans += (2*i)-1;
+			}
+			out.println(ans);
+		}
+		out.close();
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+ 
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
