c47b6d241c46db821e1edc1c49b13e2a58bdfc08
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-05-21 16:39:10 +0900

    [Bronze III] Title: 팰린드롬 소떡소떡, Time: 88 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Bronze/25630. 팰린드롬 소떡소떡/팰린드롬 소떡소떡.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	public static void main(String args[]) {
+		FastScanner sc = new FastScanner();
+		PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+		
+		int N = sc.nextInt();
+		String str = sc.next();
+		int a = 0;
+		int b = N-1;
+		int ans = 0;
+		while(a<b) {
+			if(str.charAt(a)!=str.charAt(b)) {
+				ans++;
+			}
+			a++;
+			b--;
+		}
+		out.print(ans);
+		out.close();
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+ 
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
