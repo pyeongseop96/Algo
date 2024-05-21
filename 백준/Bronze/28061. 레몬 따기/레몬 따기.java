5b43f09519e8bd15c514fec8a75f682bb2b28154
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-05-21 16:36:37 +0900

    [Bronze III] Title: 레몬 따기, Time: 300 ms, Memory: 40216 KB -BaekjoonHub

---
+++ 백준/Bronze/28061. 레몬 따기/레몬 따기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	public static void main(String args[]) {
+		FastScanner sc = new FastScanner();
+		PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+		
+		int N = sc.nextInt();
+		int ans = 0;
+		for(int i=N; i>=1; i--) {
+			int num = sc.nextInt()-i;
+			if(ans<num) {
+				ans = num;
+			}
+		}
+		out.print(ans);
+		out.close();
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+ 
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
