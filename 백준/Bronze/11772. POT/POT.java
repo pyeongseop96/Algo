2b2d39262e7ae568ab31b8540fd28b7562c44ee0
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-05-21 16:41:45 +0900

    [Bronze III] Title: POT, Time: 80 ms, Memory: 11668 KB -BaekjoonHub

---
+++ 백준/Bronze/11772. POT/POT.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	public static void main(String args[]) {
+		FastScanner sc = new FastScanner();
+		PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+		
+		int N = sc.nextInt();
+		long ans = 0;
+		for(int i=1; i<=N; i++) {
+			int num = sc.nextInt();
+			int num2 = num/10;
+			int num3 = num%10;
+			ans += Math.pow(num2, num3);
+		}
+		out.print(ans);
+		out.close();
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+ 
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
