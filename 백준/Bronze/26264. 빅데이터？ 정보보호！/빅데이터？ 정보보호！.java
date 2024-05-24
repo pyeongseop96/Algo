b89a4d3d4cf5150f50e4345c154ca924f0aecd00
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-05-24 14:20:59 +0900

    [Bronze III] Title: 빅데이터? 정보보호!, Time: 172 ms, Memory: 20152 KB -BaekjoonHub

---
+++ 백준/Bronze/26264. 빅데이터？ 정보보호！/빅데이터？ 정보보호！.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	public static void main(String args[]) {
+		FastScanner sc = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		
+		int N = sc.nextInt();
+		String str = sc.next();
+		double lulu = 7.5 * N;
+		if(str.length()<lulu) {
+			out.println("bigdata?");
+		}
+		else if(str.length()>lulu) {
+			out.println("security!");
+		}
+		else {
+			out.println("bigdata? security!");
+		}
+		out.close();
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+ 
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
