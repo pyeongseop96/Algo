4e17af2a121e7d07ec07cb794bdefdcfe98ec791
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-05-30 13:18:46 +0900

    [Bronze III] Title: 과 조사하기, Time: 80 ms, Memory: 11716 KB -BaekjoonHub

---
+++ 백준/Bronze/28289. 과 조사하기/과 조사하기.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Main {
+
+    void solve() {
+    	
+    }
+
+    void run() {
+        sc = new FastScanner();
+        out = new PrintWriter(System.out);
+//        int T = sc.nextInt();
+//        while (T-- > 0) {
+//            solve();
+//        }
+        int N = sc.nextInt();
+        int[] arr = new int[4];
+        for(int i=1; i<=N; i++) {
+        	int a = sc.nextInt();
+        	int b = sc.nextInt();
+        	int c = sc.nextInt();
+        	if(a==1) {
+        		arr[3]++;
+        	}
+        	else {
+        		if(b==1 || b==2) {
+        			arr[0]++;
+        		}
+        		else if(b==3) {
+        			arr[1]++;
+        		}
+        		else {
+        			arr[2]++;
+        		}
+        	}
+        }
+        for(int i:arr) {
+        	out.println(i);
+        }
+        out.close();
+    }
+
+    FastScanner sc;
+    PrintWriter out;
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+ 
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
