1b8bcc539ceba8160e85fa60639e1ffb4a4e003e
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-06-04 16:40:10 +0900

    [Bronze III] Title: Collatz Conjecture, Time: 84 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Bronze/13236. Collatz Conjecture/Collatz Conjecture.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    void solve() {
+
+    }
+    
+    void run() {
+        sc = new FastScanner();
+        out = new PrintWriter(System.out);
+//        int T = sc.nextInt();
+//        while (T-- > 0) {
+//            solve();
+//        }
+        int N = sc.nextInt();
+        while(true) {
+        	out.print(N);
+        	out.print(" ");
+        	if(N==1) {
+        		break;
+        	}
+        	else if(N%2==0) {
+        		N /= 2;
+        	}
+        	else {
+        		N = (N*3)+1;
+        	}
+        }
+        out.close();
+    }
+
+    FastScanner sc;
+    PrintWriter out;
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+ 
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
