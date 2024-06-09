7137b1465f7a545a2be0b3350dee4d91560b428a
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-06-09 11:23:54 +0900

    [Bronze III] Title: FizzBuzz, Time: 80 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Bronze/14761. FizzBuzz/FizzBuzz.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+    void solve() {
+    }
+    
+    void run() {
+        sc = new FastScanner();
+        out = new PrintWriter(System.out);
+//        int T = sc.nextInt();
+//        while (T-- > 0) {
+//            solve();
+//        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++) {
+        	boolean hello = true;
+        	if(i%a==0) {
+        		out.print("Fizz");
+        		hello = false;
+        	}
+        	if(i%b==0) {
+        		out.print("Buzz");
+        		hello = false;
+        	}
+        	if(hello) {
+        		out.println(i);
+        	}
+        	else {
+        		out.println();
+        	}
+        }
+        out.close();
+    }
+
+    FastScanner sc;
+    PrintWriter out;
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+ 
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+ 
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+ 
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+ 
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
