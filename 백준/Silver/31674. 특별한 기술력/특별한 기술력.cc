4470b7b936518997f65fc780d7f9a573a1a16630
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-07-05 00:55:24 +0900

    [Silver II] Title: 특별한 기술력, Time: 40 ms, Memory: 5152 KB -BaekjoonHub

---
+++ 백준/Silver/31674. 특별한 기술력/특별한 기술력.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+#define pb push_back
+#define all(a) a.begin(), a.end()
+
+#define dbg(...) cerr << "#" << __LINE__ << ' ' << #__VA_ARGS__ << "- ", debug_print(__VA_ARGS__)
+template<typename T> void debug_print(vector<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(unordered_set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(T && x) {cerr << x << endl;}
+template<typename T, typename ...S> void debug_print(T && x, S&&...y) {cerr << x << ", "; debug_print(y...);}
+ 
+template<typename T> void print(vector<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(unordered_set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(T && x) {cout << x << "\n";}
+template<typename T, typename... S> void print(T && x, S&&... y) {cout << x << ' ';print(y...);}
+
+void solve() {
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    //  int T; cin >> T;
+    //  while(T--){
+    //      solve();
+    //  }   
+    int N; cin >> N;
+    vector<ll> v(N);
+    vector<ll> w(N);
+    ll cur = 1;
+    w[0] = 1;
+    for(int i=1; i<N; i++){
+        cur = (cur*2)%1000000007;
+        w[i] = cur;
+    }
+    for(int i=0; i<N; i++){
+        cin >> v[i];
+    }
+    sort(all(v));
+    long long ans = 0;
+    for(int i=N-1; i>=0; i--){
+        ans = (ans + (v[i]*w[i]))%1000000007;
+    }
+    print(ans);
+}
