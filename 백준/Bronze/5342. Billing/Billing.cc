2b635401b95356cdb4504fa8ba7038ad19372839
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-07-11 15:30:04 +0900

    [Bronze III] Title: Billing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5342. Billing/Billing.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+#define pb push_back
+#define all(a) a.begin(), a.end()
+
+#define dbg(...) cerr << "#" << __LINE__ << ' ' << #__VA_ARGS__ << "- ", debug_print(__VA_ARGS__)
+template<typename T> void debug_print(vector<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(unordered_set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(T && x) {cerr << x << endl;}
+template<typename T, typename ...S> void debug_print(T && x, S&&...y) {cerr << x << ", "; debug_print(y...);}
+ 
+template<typename T> void print(vector<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(unordered_set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(T && x) {cout << x << "\n";}
+template<typename T, typename... S> void print(T && x, S&&... y) {cout << x << ' ';print(y...);}
+
+void solve() {
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    //  int T; cin >> T;
+    //  while(T--){
+    //      solve();
+    //  }  
+    long double sum = 0;
+    while(true){
+        string str; cin >> str;
+        if(str=="Paper"){
+            sum += 57.99;
+        }
+        if(str=="Printer"){
+            sum += 120.50;
+        }
+        if(str=="Planners"){
+            sum += 31.25;
+        }
+        if(str=="Binders"){
+            sum += 22.50;
+        }
+        if(str=="Calendar"){
+            sum += 10.95;
+        }
+        if(str=="Notebooks"){
+            sum += 11.20;
+        }
+        if(str=="Ink"){
+            sum += 66.95;
+        }
+        if(str=="EOI"){
+            break;
+        }
+    }
+    cout << "$" << sum;
+}
