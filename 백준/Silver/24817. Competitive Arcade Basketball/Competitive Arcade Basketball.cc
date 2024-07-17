9f1dc7e41c2a47a075f49f7310a1cba5a1c425ac
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-07-17 18:08:22 +0900

    [Silver V] Title: Competitive Arcade Basketball, Time: 160 ms, Memory: 9948 KB -BaekjoonHub

---
+++ 백준/Silver/24817. Competitive Arcade Basketball/Competitive Arcade Basketball.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+#define pb push_back
+#define all(a) a.begin(), a.end()
+
+#define dbg(...) cerr << "#" << __LINE__ << ' ' << #__VA_ARGS__ << "- ", debug_print(__VA_ARGS__)
+template<typename T> void debug_print(vector<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(unordered_set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(T && x) {cerr << x << endl;}
+template<typename T, typename ...S> void debug_print(T && x, S&&...y) {cerr << x << ", "; debug_print(y...);}
+ 
+template<typename T> void print(vector<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(unordered_set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(T && x) {cout << x << "\n";}
+template<typename T, typename... S> void print(T && x, S&&... y) {cout << x << ' ';print(y...);}
+
+void solve() {
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    //  int T; cin >> T;
+    //  while(T--){
+    //      solve();
+    //  }  lk
+
+    int a,b,c; cin >> a >> b >> c;
+    string tmp;
+    bool nw = true;
+    for(int i=1; i<=a; i++){
+        cin >> tmp;
+    }
+    map<string,int> m;
+    for(int i=1; i<=c; i++){
+        cin >> tmp;
+        int num; cin >> num;
+        if(m[tmp]<b){
+            m[tmp] += num;
+            if(m[tmp]>=b){
+                cout << tmp << " wins!" << '\n';
+                nw = false;
+            }
+        }
+    }
+    if(nw){
+        cout << "No winner!";
+    }
+}
