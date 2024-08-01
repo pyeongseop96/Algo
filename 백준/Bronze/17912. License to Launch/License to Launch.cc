17072666c053c123961615980150313d1ea72bdc
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-08-01 12:31:01 +0900

    [Bronze III] Title: License to Launch, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17912. License to Launch/License to Launch.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+#define pb push_back
+#define all(a) a.begin(), a.end()
+
+#define dbg(...) cerr << "#" << __LINE__ << ' ' << #__VA_ARGS__ << "- ", debug_print(__VA_ARGS__)
+template<typename T> void debug_print(vector<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(unordered_set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(T && x) {cerr << x << endl;}
+template<typename T, typename ...S> void debug_print(T && x, S&&...y) {cerr << x << ", "; debug_print(y...);}
+ 
+template<typename T> void print(vector<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(unordered_set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(T && x) {cout << x << "\n";}
+template<typename T, typename... S> void print(T && x, S&&... y) {cout << x << ' ';print(y...);}
+
+void solve() {
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    //  int T; cin >> T;
+    //  while(T--){
+    //      solve();
+    //  }  lk
+
+    int N; cin >> N;
+    int min = 1000000009;
+    int idx = 0;
+    for(int i=0; i<N; i++){
+        int tmp; cin >> tmp;
+        if(min > tmp){
+            min = tmp;
+            idx = i;
+        }
+    }
+    cout << idx;
+}
