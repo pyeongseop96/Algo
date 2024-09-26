7e1a089f59e8037dcb48cb6144b47774bdeef03a
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-09-26 19:21:23 +0900

    [Bronze III] Title: 문자열을 만들어요, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/32297. 문자열을 만들어요/문자열을 만들어요.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+#define pb push_back
+#define all(a) a.begin(), a.end()
+
+#define dbg(...) cerr << "#" << __LINE__ << ' ' << #__VA_ARGS__ << "- ", debug_print(__VA_ARGS__)
+template<typename T> void debug_print(vector<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(unordered_set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void debug_print(T && x) {cerr << x << endl;}
+template<typename T, typename ...S> void debug_print(T && x, S&&...y) {cerr << x << ", "; debug_print(y...);}
+ 
+template<typename T> void print(vector<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(unordered_set<T> x){for(auto i: x) cout << i << ' ';cout << "\n";}
+template<typename T> void print(T && x) {cout << x << "\n";}
+template<typename T, typename... S> void print(T && x, S&&... y) {cout << x << ' ';print(y...);}
+
+void solve() {
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    //  int T; cin >> T;
+    //  while(T--){
+    //      solve();
+    //  }
+    
+    
+    int N; cin >> N;
+    string str; cin >> str;
+    string ans = "NO";
+    for(int i=3; i<N; i++){
+        if(str[i-3]=='g' &&
+        str[i-2]=='o' &&
+        str[i-1]=='r' &&
+        str[i]=='i'){
+            ans = "YES";
+            break;
+        }
+    }
+    print(ans);
+
+}
