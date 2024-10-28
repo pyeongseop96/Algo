b72369b59eb2326e1a3bbb4e00a18acae1fd9d55
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-10-28 19:32:13 +0900

    [Bronze II] Title: 콘테스트, Time: 96 ms, Memory: 12868 KB -BaekjoonHub

---
+++ 백준/Bronze/5576. 콘테스트/콘테스트.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr1 = new int[10];
+        int[] arr2 = new int[10];
+        for(int i=0; i<10; i++) {
+        	arr1[i] = sc.nextInt();
+        }
+        for(int i=0; i<10; i++) {
+        	arr2[i] = sc.nextInt();
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        System.out.print(arr1[9]+arr1[8]+arr1[7]);
+        System.out.print(" ");
+        System.out.print(arr2[9]+arr2[8]+arr2[7]);
+    }
+}
