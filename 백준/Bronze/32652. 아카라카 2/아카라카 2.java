5f83ce4b01eb5ec6bb52e6f060ddcf24c3b720da
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2024-11-22 21:36:48 +0900

    [Bronze III] Title: 아카라카 2, Time: 96 ms, Memory: 13012 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.print("AKARAKA");
+        for(int i=2; i<=N; i++) {
+        	System.out.print("RAKA");
+        }
+    }
+}
