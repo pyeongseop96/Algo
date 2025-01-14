5f4c3f8f5edc917fd7ef6dd84be4a9b75c7de13d
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2025-01-15 08:25:26 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define precision(x) cout << fixed; cout.precision(x);
+#define all(a) begin(a), end(a)
+
+/* 보석함
+ * cout << (s.find("fgg") == string::npos) << endl;
+ * ceil, round, floor, sqrt
+ * multiset<string> ss;
+    cout << *s.rbegin() << endl;
+ * vector<int> c(n);
+    cout << max_element(c.begin(), c.end()) - c.begin()
+ * bitset<1000> s;
+ */
+
+int main() {
+    int a,b; cin >> a >> b;
+    cout << 1000*a+10000*b << endl;
+}
