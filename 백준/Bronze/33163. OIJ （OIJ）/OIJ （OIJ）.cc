aad32bdc8f50497109c7a9bd3c8fd7a548a6b4ac
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2025-01-17 08:28:05 +0900

    [Bronze IV] Title: OIJ (OIJ), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/33163. OIJ （OIJ）/OIJ （OIJ）.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define precision(x) cout << fixed; cout.precision(x);
+#define all(a) begin(a), end(a)
+
+/* 보석함
+ * cout << (s.find("fgg") == string::npos) << endl;
+ * ceil, round, floor, sqrt
+ * multiset<string> ss;
+    cout << *s.rbegin() << endl;
+ * vector<int> c(n);
+    cout << max_element(c.begin(), c.end()) - c.begin()
+ * bitset<1000> s;
+ */
+
+int main() {
+    string a,b; cin >> a >> b;
+    for(char c:b){
+        if(c=='J') cout << 'O';
+        else if(c=='O') cout << 'I';
+        else cout << 'J';
+    }
+}
