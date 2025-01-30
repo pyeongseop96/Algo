fc15a33a6846659a0671d9ee3b11b272f0f42062
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2025-01-30 16:59:34 +0900

    [Bronze III] Title: In the Shower, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11176. In the Shower/In the Shower.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define precision(x) cout << fixed; cout.precision(x);
+#define all(a) begin(a), end(a)
+
+/* 보석함
+ * cout << (s.find("fgg") == string::npos) << endl;
+ * ceil, round, floor, sqrt
+ * multiset<string> ss;
+    cout << *s.rbegin() << endl;
+ * vector<int> c(n);
+    cout << max_element(c.begin(), c.end()) - c.begin()
+ * bitset<1000> s;
+
+ * stringstream ss(input);
+ */
+
+int main() {
+   int N; cin >> N;
+   for (int i = 1; i <= N; i++) {
+      int a,b; cin >> a >> b;
+      int ans = 0;
+      for (int j=1; j<=b; j++) {
+         int temp; cin >> temp;
+         if (temp > a) {
+            ans++;
+         }
+      }
+      cout << ans << endl;
+   }
+}
