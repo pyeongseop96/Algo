15795624dd89abb4451bd7734666ffd6ed2e9857
pyeongseop96 <148199848+pyeongseop96@users.noreply.github.com>
2025-01-30 16:42:56 +0900

    [Bronze III] Title: Abdelrahman, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10693. Abdelrahman/Abdelrahman.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define precision(x) cout << fixed; cout.precision(x);
+#define all(a) begin(a), end(a)
+
+/* 보석함
+ * cout << (s.find("fgg") == string::npos) << endl;
+ * ceil, round, floor, sqrt
+ * multiset<string> ss;
+    cout << *s.rbegin() << endl;
+ * vector<int> c(n);
+    cout << max_element(c.begin(), c.end()) - c.begin()
+ * bitset<1000> s;
+
+ * stringstream ss(input);
+ */
+
+int main() {
+   int N; cin >> N;
+   for (int i = 1; i <= N; i++) {
+      int a,b; cin >> a >> b;
+      cout << "Case " << i << ": " << b-a+1 << endl;
+   }
+}
